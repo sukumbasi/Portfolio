5ed2c9d7e69a08d57e9dc8b59fa9c5ca78a272ab
sukumbasi <111kkhatiwada@gmail.com>
2024-06-16 16:23:27 +0545

    KK

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ function Header() {
         <a href='https://www.facebook.com/profile.php?id=100071210860039'><img src={fb} alt="fb" height='30' width='30'/></a>
         <a href ='https://www.linkedin.com/in/kaushal-khatiwada-61ba52301/'><img src={linkedln} alt="linkedln" height='30' width='30'/></a>
         <a href ='https://www.youtube.com/@KaushalKhatiwada_11'> <img src={youtube} alt="medium" height='30' width='30'/> </a>
-        <a href='https://medium.com/@kaushalkhatiwada11'><img src={medium} alt="medium" height='30' width='30'/></a>
+        <a href='https://kaushalkhatiwada11.medium.com'><img src={medium} alt="medium" height='30' width='30'/></a> 
         <a href='https://github.com/KaushalKhatiwada'><img src={github}  alt="github" height='30' width='30'/></a> 
         <a href='https://huggingface.co/Kaushal-Khatiwada'><img src={huggingface} alt="huggingface" height='30' width='30'/></a>
         <a href='https://www.kaggle.com/kaushalkhatiwada11'><img src={kaggle} alt="kaggle" height='30' width='30'/></a>
